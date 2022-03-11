0d0dfc4c016f870bacc0bf295959f5de4c48806c
gokul7019 <gksgokul714319@gmail.com>
2022-03-11 06:22:11 +0000

    Final Performance Queries

--- Performance_queries_GOKUL_KANNAN_G/Performance _query2_Gokul_kannan_G.sql
+++ Performance_queries_GOKUL_KANNAN_G/Performance _query2_Gokul_kannan_G.sql
@@ -1,9 +1,14 @@
-select CAND_NAME FROM Election WHERE ST_NAME = 'Andaman & Nicobar Islands' and Partyabbre = 'BJP' and year = 1988; 
+--1. Display the BJP candidate list participated at state Andaman & Nicobar Islands in the year 1984?
+select CAND_NAME FROM Election WHERE ST_NAME = 'Andaman & Nicobar Islands' and Partyabbre = 'BJP' and year = 1988;
 
+--2. How many Male Candidates participated in elections in each state?
 SELECT COUNT(CAND_NAME), ST_NAME FROM ELECTION WHERE CAND_SEX = 'M' GROUP BY ST_NAME;
 
+--3. Display the Party name in descending order?
 SELECT PARTYNAME FROM ELECTION GROUP BY PARTYNAME ORDER BY PARTYNAME DESC;
 
+--4. Find how many Candidates participated in the year West Bengal 1988?
 SELECT COUNT(CAND_NAME) FROM ELECTION WHERE ST_NAME = 'West Bengal' and year = 1988;
 
+--5. Display the Tamilnadu Candidate list each year?
 SELECT CAND_NAME , year FROM ELECTION WHERE ST_NAME = 'Tamil Nadu' order by  year;
--- Performance_queries_GOKUL_KANNAN_G/Performance_query3_Gokul_Kannan_G.sql
+++ Performance_queries_GOKUL_KANNAN_G/Performance_query3_Gokul_Kannan_G.sql
@@ -1,10 +1,14 @@
+--1. Write a SQL query to calculate total votes Got BJP in Bihar State in 1996?
 select sum(TOTVOTPOLL) as TOTAL_VOTES FROM ELECTION WHERE PARTYABBRE = 'BJP' AND YEAR = 1996 AND ST_NAME = 'Bihar';
 
+--2. Write a SQL query to display the BJP Candidates list in descending order?
 SELECT DISTINCT(CAND_NAME) FROM ELECTION WHERE PARTYABBRE = 'BJP' ORDER BY CAND_NAME DESC;
 
+--3. Find how many times congress gets above 50% vote in Bihar state?
 SELECT count(*) FROM ELECTION WHERE ST_NAME = 'Bihar' AND PARTYABBRE = 'INC' and (TOTVOTPOLL)/(ELECTORS) * 100 > 20;
 
-
+--4. Find the Sikkim Candidate list each year?
 SELECT CAND_NAME AS CANDIDATE_NAME , YEAR FROM ELECTION WHERE ST_NAME = 'Sikkim' order by year ;
 
+--5. Find total male candidates who participated in the election?
 SELECT CAND_NAME AS MALE_CANIDIATE FROM ELECTION WHERE CAND_SEX = 'M';
